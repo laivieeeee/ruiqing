4baf0c20c27d299bb3e131e27959d3e2a0b3f70f
pactera-479 <707944442@qq.com>
2019-12-16 14:34:11 +0800

    新增redis分布式锁工具类

---
+++ code/ruiqing/rq-common/src/main/java/com/ruiqing/common/utils/RedisLockUtil.java
@@ -0,0 +1,66 @@
+package com.ruiqing.common.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
+
+/**
+ * REDIS分布式锁工具类
+ */
+public class RedisLockUtil {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RedisLockUtil.class);
+	
+	@SuppressWarnings("unchecked")
+	private static RedisTemplate<String, Object> redisTemplate = SpringUtils.getBean("redisTemplate",
+			RedisTemplate.class);
+	
+	private static long lockTimeout = 5*60*1000; //锁定时间
+
+	/**
+     * 加锁
+     *
+     * @param key
+     * @param value 当前时间+ 超时时间
+     * @return
+     */
+    public static boolean lock(String key, long currvalue) {
+    	String value = currvalue+lockTimeout+"";
+    	//未加锁，则加锁
+        if (redisTemplate.opsForValue().setIfAbsent(key, value)) {
+            return true;
+        }
+        
+        //获取锁解锁时间
+        String currentValue = (String)redisTemplate.opsForValue().get(key);
+        //如果锁过期
+        if (!StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+            //获得上一个锁的时间
+            String olcValue = (String)redisTemplate.opsForValue().getAndSet(key, value);
+            if (!StringUtils.isEmpty(olcValue) && olcValue.equals(currentValue)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 解锁
+     *
+     * @param key
+     * @param value
+     */
+    public static void unlock(String key, long currvalue) {
+        try {
+        	String value = currvalue+lockTimeout+"";
+            String currentVlue = (String)redisTemplate.opsForValue().get(key);
+            if (!StringUtils.isEmpty(currentVlue) && currentVlue.equals(value)) {
+                redisTemplate.opsForValue().getOperations().delete(key);
+            }
+        } catch (Exception e) {
+        	logger.error("【redis分布式锁】 解锁异常" + e.getMessage());
+        }
+    }
+}
