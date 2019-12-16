de1f5ba5f2f082239087bce8bc641c5b51f76096
pactera-479 <707944442@qq.com>
2019-12-16 15:16:54 +0800

    1.aesutil加解密

---
+++ code/ruiqing/rq-common/src/main/java/com/ruiqing/common/utils/AESUtil.java
@@ -0,0 +1,117 @@
+package com.ruiqing.common.utils;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * AES加解密工具类
+ */
+public class AESUtil {
+
+	public static final String KEY = "h1k2#3s4f5d6%7d8";//秘钥
+	
+	/**
+	 * AES的CBC加密模式，默认iv是16个0（这个称为初始化向量），由于是分组加密，所以下一组的iv，就用前一组的加密的密文来充当
+	 * CFB、OFB模式类似，只不过更复杂，加大破解难度
+	 */
+	public static final String IV = "h1k2#3s4f5d6%7d8";//初始化向量
+
+
+	 /**
+     * 加密方法
+     * @param data  要加密的数据
+     * @param key 加密key
+     * @param iv 加密iv
+     * @return 加密的结果
+     * @throws Exception
+     */
+    public static String encrypt(String data, String key, String iv) throws Exception {
+        try {
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");//"算法/模式/补码方式"
+            int blockSize = cipher.getBlockSize();
+
+            byte[] dataBytes = data.getBytes();
+            int plaintextLength = dataBytes.length;
+            if (plaintextLength % blockSize != 0) {
+                plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize));
+            }
+
+            byte[] plaintext = new byte[plaintextLength];
+            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+
+            cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
+            byte[] encrypted = cipher.doFinal(plaintext);
+
+            return new Base64().encodeToString(encrypted);
+
+        } catch (Exception e) {
+            throw new RuntimeException("encrypt",e);
+        }
+    }
+
+    /**
+     * 解密方法
+     * @param data 要解密的数据
+     * @param key  解密key
+     * @param iv 解密iv
+     * @return 解密的结果
+     * @throws Exception
+     */
+    public static String decrpyt(String data, String key, String iv) throws Exception {
+        try {
+            byte[] encrypted1 = new Base64().decode(data);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+
+            cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original);
+            return originalString;
+        } catch (Exception e) {
+        	throw new RuntimeException("decrpyt error",e);
+        }
+    }
+    
+    /**
+     * 使用默认的key和iv加密
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    public static String encrypt(String data) throws Exception {
+        return encrypt(data, KEY, IV);
+    }
+
+    /**
+     * 使用默认的key和iv解密
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    public static String decrpyt(String data) throws Exception {
+        return decrpyt(data, KEY, IV);
+    }
+    
+
+	public static void main(String[] args) throws Exception {
+
+		String ddd = "{ \"userCode\":\"wx\",\"password\":\"123456\",\"captcha\":\"123\"}";
+		//加密
+		String str = encrypt(ddd);
+		System.out.println(str);
+		//解密
+		String dstr=decrpyt(str);
+		System.out.println(dstr);
+	}
+
+}
